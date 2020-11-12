011baaf45afcdc193c4ea880116a24442d5db983
KBR9 <cabero96@protonmail.com>
2020-11-12 19:46:30 +0100

    add utility trait

--- organize_core/src/utils/mod.rs
+++ organize_core/src/utils/mod.rs
@@ -3,7 +3,6 @@ pub mod tests {
 	use std::{env, path::PathBuf};
 
 	pub fn project() -> PathBuf {
-		// when 'cargo test' is run, the current directory should be the project directory
 		env::current_dir().unwrap().parent().unwrap().to_path_buf()
 	}
 }
@@ -12,8 +11,18 @@ pub trait UnwrapRef<T> {
 	fn unwrap_ref(&self) -> &T;
 }
 
+pub trait UnwrapMut<T> {
+	fn unwrap_mut(&mut self) -> &mut T;
+}
+
 impl<T> UnwrapRef<T> for Option<T> {
 	fn unwrap_ref(&self) -> &T {
 		self.as_ref().unwrap()
+	}
+}
+
+impl<T> UnwrapMut<T> for Option<T> {
+	fn unwrap_mut(&mut self) -> &mut T {
+		self.as_mut().unwrap()
 	}
 }
