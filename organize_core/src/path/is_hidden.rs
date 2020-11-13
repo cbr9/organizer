f88f72b6edf2674bba7fce68da14159109a7fa17
KBR9 <cabero96@protonmail.com>
2020-11-13 11:00:46 +0100

    implement is_hidden() for windows targets

--- organize_core/src/path/is_hidden.rs
+++ organize_core/src/path/is_hidden.rs
@@ -1,3 +1,8 @@
+#[cfg(target_os = "windows")]
+use std::os::windows::prelude::*;
+#[cfg(target_os = "windows")]
+use winapi::um::winnt::FILE_ATTRIBUTE_HIDDEN;
+
 use std::path::Path;
 
 pub trait IsHidden {
@@ -7,12 +12,44 @@ pub trait IsHidden {
 impl IsHidden for Path {
 	#[cfg(any(target_os = "linux", target_os = "macos"))]
 	fn is_hidden(&self) -> bool {
-		self.file_name().unwrap().to_str().unwrap().starts_with('.')
+		match self.file_name() {
+			None => false,
+			Some(filename) => filename.to_string_lossy().starts_with('.'),
+		}
 	}
 
 	#[cfg(target_os = "windows")]
 	fn is_hidden(&self) -> bool {
-		// must use winapi
-		unimplemented!()
+		let metadata = std::fs::metadata(self).unwrap();
+		metadata.file_attributes() & FILE_ATTRIBUTE_HIDDEN > 0
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::path::IsHidden;
+	#[cfg(target_os = "windows")]
+	use std::os::windows::prelude::*;
+	use std::{fs, path::Path};
+	#[cfg(target_os = "windows")]
+	use winapi::um::winnt::FILE_ATTRIBUTE_HIDDEN;
+
+	#[test]
+	#[cfg(any(target_os = "linux", target_os = "macos"))]
+	fn check_hidden() {
+		let path = Path::new(".testfile");
+		assert!(path.is_hidden())
+	}
+
+	#[test]
+	#[cfg(target_os = "windows")]
+	fn check_hidden() {
+		let path = Path::new(".testfile");
+		fs::OpenOptions::new()
+			.create(true)
+			.write(true)
+			.open(path)
+			.custom_flags(FILE_ATTRIBUTE_HIDDEN);
+		assert!(path.is_hidden())
 	}
 }
