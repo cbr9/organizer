63d4fe70455977e0e75858f8c41db5268cd1bf08
cbr9 <cabero96@protonmail.com>
2020-12-04 11:47:14 +0100

    refactor and test

--- organize_core/src/data/path_to_rules.rs
+++ organize_core/src/data/path_to_rules.rs
@@ -1,4 +1,5 @@
-use crate::data::Data;
+use crate::data::config::UserConfig;
+use std::iter::FromIterator;
 use std::{
 	collections::{hash_map::Keys, HashMap},
 	path::PathBuf,
@@ -7,15 +8,19 @@ use std::{
 pub struct PathToRules<'a>(HashMap<&'a PathBuf, Vec<(usize, usize)>>);
 
 impl<'a> PathToRules<'a> {
-	pub fn new(data: &'a Data) -> Self {
-		let mut map = HashMap::with_capacity(data.config.rules.len());
-		data.config.rules.iter().enumerate().for_each(|(i, rule)| {
+	pub fn new(config: &'a UserConfig) -> Self {
+		let mut map = HashMap::with_capacity(config.rules.len()); // there will be at least one folder per rule
+		config.rules.iter().enumerate().for_each(|(i, rule)| {
 			rule.folders.iter().enumerate().for_each(|(j, folder)| {
 				let path = &folder.path;
-				if !map.contains_key(path) {
-					map.insert(path, Vec::new());
-				}
-				map.get_mut(path).unwrap().push((i, j));
+				match map.get_mut(path) {
+					None => {
+						map.insert(path, vec![(i, j)]);
+					}
+					Some(vec) => {
+						vec.push((i, j));
+					}
+				};
 			})
 		});
 		map.shrink_to_fit();
@@ -30,18 +35,59 @@ impl<'a> PathToRules<'a> {
 		self.0.get(key).unwrap_or_else(|| {
 			// if the path is some subdirectory not represented in the hashmap
 			let components = key.components().collect::<Vec<_>>();
-			let mut paths = Vec::new();
-			for i in 0..components.len() {
-				let slice = components[0..i].iter().map(|comp| comp.as_os_str().to_string_lossy()).collect::<Vec<_>>();
-				let str: String = slice.join(&std::path::MAIN_SEPARATOR.to_string());
-				paths.push(PathBuf::from(str.replace("//", "/")))
-			}
-			let path = paths
-				.iter()
+			(0..components.len())
+				.map(|i| PathBuf::from_iter(&components[0..i]))
 				.rev()
-				.find_map(|path| if self.0.contains_key(path) { Some(path) } else { None })
-				.unwrap();
-			self.0.get(path).unwrap()
+				.find_map(|path| if self.0.contains_key(&path) { Some(path) } else { None })
+				.map(|path| self.0.get(&path).unwrap())
+				.unwrap()
 		})
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::data::config::folders::Folder;
+	use crate::data::config::Rule;
+	use crate::data::options::Options;
+	use crate::utils::DefaultOpt;
+	use dirs::home_dir;
+	use std::convert::TryFrom;
+
+	#[test]
+	fn test_new() {
+		let home = home_dir().unwrap();
+		let download_dir = home.join("Downloads");
+		let document_dir = home.join("Documents");
+		let picture_dir = home.join("Pictures");
+		let rules = vec![
+			Rule {
+				folders: vec![
+					Folder::try_from(download_dir.clone()).unwrap(),
+					Folder::try_from(document_dir.clone()).unwrap(),
+				],
+				..Default::default()
+			},
+			Rule {
+				folders: vec![
+					Folder::try_from(picture_dir.clone()).unwrap(),
+					Folder::try_from(download_dir.clone()).unwrap(),
+				],
+				..Default::default()
+			},
+		];
+		let config = UserConfig {
+			rules,
+			defaults: Options::default_none(),
+		};
+
+		let value = PathToRules::new(&config).0;
+		let mut expected = HashMap::new();
+		expected.insert(&download_dir, vec![(0, 0), (1, 1)]);
+		expected.insert(&document_dir, vec![(0, 1)]);
+		expected.insert(&picture_dir, vec![(1, 0)]);
+
+		assert_eq!(value, expected)
 	}
 }
