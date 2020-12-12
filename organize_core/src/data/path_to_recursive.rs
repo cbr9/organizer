41d2ee57872b4eeea25f9571334f9e7beeafb47d
cbr9 <cabero96@protonmail.com>
2020-12-12 11:27:33 +0100

    add test for PathToRecursive::new

--- organize_core/src/data/path_to_recursive.rs
+++ organize_core/src/data/path_to_recursive.rs
@@ -8,7 +8,7 @@ use std::{
 	path::Path,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct PathToRecursive<'a>(HashMap<&'a Path, (RecursiveMode, Option<u16>)>);
 
 impl<'a> PathToRecursive<'a> {
@@ -55,4 +55,71 @@ impl<'a> PathToRecursive<'a> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+	use super::*;
+	use crate::data::options::Options;
+	use crate::data::config::{Config, Rule};
+	use crate::utils::DefaultOpt;
+	use crate::data::settings::Settings;
+	use crate::data::config::actions::Actions;
+	use crate::data::config::filters::Filters;
+	use crate::data::config::folders::Folder;
+	use crate::data::options::recursive::Recursive;
+
+	#[test]
+	fn new() {
+		let downloads = "$HOME/Downloads";
+		let documents = "$HOME/Documents";
+		let data = Data {
+			defaults: Options::default_some(),
+			settings: Settings::default_some(),
+			config: Config {
+				rules: vec![
+					Rule {
+						actions: Actions(vec![]),
+						filters: Filters {
+							inner: vec![]
+						},
+						folders: vec![
+							Folder {
+								path: downloads.clone().into(),
+								options: Options::default_none()
+							},
+							Folder {
+								path: documents.clone().into(),
+								options: Options::default_none()
+							},
+						],
+						options: Options::default_none(),
+					},
+					Rule {
+						actions: Actions(vec![]),
+						filters: Filters {
+							inner: vec![]
+						},
+						folders: vec![
+							Folder {
+								path: downloads.clone().into(),
+								options: Options {
+									recursive: Recursive {
+										enabled: Some(true),
+										depth: Some(1)
+									},
+									..DefaultOpt::default_none()
+								}
+							},
+						],
+						options: Options::default_none(),
+					},
+
+				],
+				defaults: Options::default_none(),
+			}
+		};
+        let mut expected = HashMap::new();
+		expected.insert(Path::new(downloads), (RecursiveMode::Recursive, Some(1)));
+		expected.insert(Path::new(documents), (RecursiveMode::NonRecursive, None));
+		let path_to_recursive = PathToRecursive::new(&data);
+		assert_eq!(path_to_recursive.0, expected);
+	}
+}
