b18cb5b9397b314037f311f0555c757f949fd601
cbr9 <cabero96@protonmail.com>
2020-12-26 17:45:52 +0100

    remove old test

--- organize_core/src/data/options/recursive/mod.rs
+++ organize_core/src/data/options/recursive/mod.rs
@@ -25,14 +25,3 @@ impl DefaultOpt for Recursive {
 	}
 }
 
-#[cfg(test)]
-mod tests {
-	use super::*;
-	#[test]
-	fn test_validity() {
-		let some = Recursive::default_some();
-		assert!(some.depth.is_some() && some.enabled.is_some() && some.enabled.unwrap());
-		let none = Recursive::default_none();
-		assert!(none.depth.is_none() && none.enabled.is_some() && !none.enabled.unwrap())
-	}
-}
