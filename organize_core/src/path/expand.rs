13fe89366e260ce0d368bbd9c440f111737e93ae
cbr9 <cabero96@protonmail.com>
2021-01-26 12:26:09 +0100

    refactored and fixed potential problems in expand.rs

--- organize_core/src/path/expand.rs
+++ organize_core/src/path/expand.rs
@@ -1,51 +1,59 @@
-use std::env::VarError;
+use anyhow::{Context, Result, anyhow, bail};
 use std::iter::FromIterator;
 use std::{
 	env,
 	path::{Path, PathBuf},
 };
+use std::ffi::{OsStr, OsString};
+use std::path::Component;
+use dirs::home_dir;
 
 pub trait Expand {
-	// TODO: implement for str
-	fn expand_user(self) -> Result<PathBuf, VarError>
+	fn expand_user(self) -> Result<PathBuf>
 	where
 		Self: Sized;
-	fn expand_vars(self) -> Result<PathBuf, VarError>
+	fn expand_vars(self) -> Result<PathBuf>
 	where
 		Self: Sized;
 }
 
 impl<T: Into<PathBuf>> Expand for T {
-	fn expand_user(self) -> Result<PathBuf, VarError> {
+	fn expand_user(self) -> Result<PathBuf> {
 		let path = self.into();
-		let str = path.to_string_lossy();
-		if str.contains('~') {
-			env::var("HOME").map(|home| str.replace("~", &home).into())
-		} else {
-			Ok(path)
+		let mut components = path.components();
+		if let Some(component) = components.next() {
+			if component.as_os_str() == OsStr::new("~") {
+				let mut path = home_dir().ok_or_else(|| anyhow!("could not find home directory"))?;
+				path.extend(components);
+                return Ok(path)
+			}
 		}
+        Ok(path)
 	}
 
-	fn expand_vars(self) -> Result<PathBuf, VarError> {
+	fn expand_vars(self) -> Result<PathBuf> {
 		let path = self.into();
 		let str = path.to_string_lossy();
 		if str.contains('$') {
-			let mut components = Vec::new();
+			let mut new_components = Vec::with_capacity(path.components().count());
 			for comp in path.components() {
-				let component: &Path = comp.as_ref();
-				let component = component.to_string_lossy();
-				if component.starts_with('$') {
-					env::var(component.replace('$', "")).map(|comp| components.push(comp))?;
+				let component_path: &Path = comp.as_ref();
+				let component_str = component_path.to_string_lossy();
+				if component_str.starts_with('$') {
+					let key = component_str.replace('$', "");
+					let value = env::var_os(&key).with_context(|| format!("could not find ${} environment variable", key))?;
+					new_components.push(value);
 				} else {
-					components.push(component.to_string());
+					let str = OsString::from(component_path);
+					new_components.push(str);
 				}
 			}
 			if str.ends_with('/') {
-				if let Some(last) = components.last_mut() {
-					last.push('/')
+				if let Some(last) = new_components.last_mut() {
+					last.push("/")
 				}
 			}
-			Ok(PathBuf::from_iter(components))
+			Ok(PathBuf::from_iter(new_components))
 		} else {
 			Ok(path)
 		}
@@ -63,28 +71,34 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn user() {
-		let original = PathBuf::from("~/Documents");
+	fn invalid_tilde() {
+		let original = home_dir().unwrap().join("Documents~");
+		assert_eq!(original.clone().expand_user().unwrap(), original)
+	}
+
+	#[test]
+	fn user_tilde() {
+		let original = "~/Documents";
 		let expected = home_dir().unwrap().join("Documents");
 		assert_eq!(original.expand_user().unwrap(), expected)
 	}
 	#[test]
 	fn home() {
-		let original = PathBuf::from("$HOME/Documents");
+		let original = "$HOME/Documents";
 		let expected = home_dir().unwrap().join("Documents");
 		assert_eq!(original.expand_vars().unwrap(), expected)
 	}
 	#[test]
 	fn new_var() {
-		env::set_var("PROJECT_DIR", project());
-		let original = PathBuf::from("$PROJECT_DIR/tests");
+        let var = "PROJECT_DIR";
+		env::set_var(var, project());
+		let original = format!("${}/tests", var);
 		let expected = project().join("tests");
 		assert_eq!(original.expand_vars().unwrap(), expected)
 	}
 	#[test]
 	fn non_existing_var() {
-		let var = "PROJECT_DIR_2";
-		let tested = PathBuf::from(format!("${}/tests", var));
+		let tested = "$NON_EXISTING_VAR/tests";
 		assert!(tested.expand_vars().is_err())
 	}
 }
