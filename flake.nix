eb3a3b81d459d17f9ed2d74ffaffcdceea79a1f6
cbr9 <cabero96@protonmail.com>
2023-10-18 19:09:34 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -5,33 +5,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) { inherit system; };
-        naersk' = pkgs.callPackage naersk { };
-        lib = nixpkgs.lib;
-      in {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          copyBins = true;
-          src = pkgs.fetchFromGitHub {
-            owner = "cbr9";
-            repo = "organizer";
-            rev = "c25ab3d";
-            sha256 = "sha256-FoQD9I21r/Lux/kiB87zrzb6yqqY91eIfO8n3Zznjug=";
-          };
-        };
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = (import nixpkgs) {inherit system;};
+      naersk' = pkgs.callPackage naersk {};
+    in {
+      # For `nix build` & `nix run`:
+      defaultPackage = naersk'.buildPackage {
+        copyBins = true;
+        src = ./.;
+      };
 
-        # For `nix develop` (optional, can be skipped):
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
-            clippy
-            rust-analyzer
-            rustfmt
-          ];
-        };
-      });
+      # For `nix develop` (optional, can be skipped):
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rustc
+          cargo
+          clippy
+          rust-analyzer
+          rustfmt
+        ];
+      };
+    });
 }
