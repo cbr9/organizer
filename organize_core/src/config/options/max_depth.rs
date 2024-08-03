f01688efaa83be45a1c1a11b9be1002287212bae
cbr9 <cabero96@protonmail.com>
2024-08-03 18:40:43 +0200

    remove dead code

--- organize_core/src/config/options/max_depth.rs
+++ organize_core/src/config/options/max_depth.rs
@@ -18,31 +18,10 @@ impl MaxDepth {
 
 		WalkDir::new(path).min_depth(1).max_depth(max_depth as usize)
 	}
-
-	pub fn type_(&self) -> notify::RecursiveMode {
-		if self.0 != 1 {
-			notify::RecursiveMode::Recursive
-		} else {
-			notify::RecursiveMode::NonRecursive
-		}
-	}
 }
 
 impl Default for MaxDepth {
 	fn default() -> Self {
 		MaxDepth(1)
-	}
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-	// use notify::RecursiveMode::*;
-
-	#[test]
-	fn is_recursive() {
-		assert!(MaxDepth(1).type_() == notify::RecursiveMode::NonRecursive);
-		assert!(MaxDepth(0).type_() == notify::RecursiveMode::Recursive);
-		assert!(MaxDepth(2).type_() == notify::RecursiveMode::Recursive);
 	}
 }
