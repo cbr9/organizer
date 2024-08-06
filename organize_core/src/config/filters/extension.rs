0fcb58210209f882ba504f6e2c84b8519547134e
Andrés Cabero <cabero96@protonmail.com>
2024-08-06 19:42:34 +0200

    fix extension filter + add ability to negate individual extensions

--- organize_core/src/config/filters/extension.rs
+++ organize_core/src/config/filters/extension.rs
@@ -10,15 +10,25 @@ pub struct Extension {
 
 impl AsFilter for Extension {
 	fn matches(&self, res: &Resource) -> bool {
-		let path = &res.path;
-		if path.is_file() {
-			return path
-				.extension()
-				.and_then(|ext| ext.to_str())
-				.map(|s| self.extensions.contains(&s.to_string()))
-				.unwrap_or(false);
+		let extension = res.path.extension().unwrap_or_default().to_string_lossy().to_string();
+		if extension.is_empty() {
+			return false;
 		}
-		true
+
+		self.extensions.iter().any(|e| {
+			let mut negate = false;
+			let mut parsed = e.clone();
+
+			if parsed.starts_with('!') {
+				negate = true;
+				parsed = parsed.replacen('!', "", 1);
+			}
+			let mut matches = parsed == extension;
+			if negate {
+				matches = !matches
+			}
+			matches
+		})
 	}
 }
 
@@ -35,16 +45,16 @@ pub mod tests {
 		let extension = Extension {
 			extensions: vec!["pdf".into()],
 		};
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
-		assert!(extension.matches(&mut path))
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		assert!(extension.matches(&path))
 	}
 	#[test]
 	fn multiple_match_pdf() {
 		let extension = Extension {
 			extensions: vec!["pdf".into(), "doc".into(), "docx".into()],
 		};
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
-		assert!(extension.matches(&mut path))
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		assert!(extension.matches(&path))
 	}
 
 	#[test]
@@ -52,7 +62,7 @@ pub mod tests {
 		let extension = Extension {
 			extensions: vec!["pdf".into(), "doc".into(), "docx".into()],
 		};
-		let mut path = Resource::from_str("$HOME/Downloads/test.jpg").unwrap();
-		assert!(!extension.matches(&mut path))
+		let path = Resource::from_str("$HOME/Downloads/test.jpg").unwrap();
+		assert!(!extension.matches(&path))
 	}
 }
