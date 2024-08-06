e82220e6f49dc2c958707222c43edd2f30d5a251
cbr9 <cabero96@protonmail.com>
2024-08-06 21:33:57 +0200

    refactor filename filter

--- organize_core/src/config/filters/filename.rs
+++ organize_core/src/config/filters/filename.rs
@@ -1,4 +1,4 @@
-use crate::{config::filters::AsFilter, resource::Resource};
+use crate::{config::filters::AsFilter, resource::Resource, templates::TERA};
 use serde::Deserialize;
 
 // TODO: refactor
@@ -6,41 +6,81 @@ use serde::Deserialize;
 #[derive(Eq, PartialEq, Deserialize, Debug, Clone, Default)]
 #[serde(deny_unknown_fields)]
 pub struct Filename {
-	pub startswith: Option<String>,
-	pub endswith: Option<String>,
-	pub contains: Option<String>,
+	#[serde(default)]
+	pub startswith: Vec<String>,
+	#[serde(default)]
+	pub endswith: Vec<String>,
+	#[serde(default)]
+	pub contains: Vec<String>,
 	#[serde(default)]
 	pub case_sensitive: bool,
 }
 
 impl AsFilter for Filename {
 	fn matches(&self, res: &Resource) -> bool {
-		let path = &res.path;
-		let mut filename = path.file_name().unwrap().to_str().unwrap().to_string();
-		let mut filter = self.clone();
-		if !self.case_sensitive {
-			filename = filename.to_lowercase();
-			if let Some(startswith) = &filter.startswith {
-				filter.startswith = Some(startswith.to_lowercase())
-			}
-			if let Some(endswith) = &filter.endswith {
-				filter.endswith = Some(endswith.to_lowercase())
-			}
-			if let Some(contains) = &filter.contains {
-				filter.contains = Some(contains.to_lowercase())
-			}
+		let filename = res.path.file_name().unwrap_or_default().to_string_lossy().to_string();
+		if filename.is_empty() {
+			return false;
 		}
-		let mut matches = true;
-		if let Some(startswith) = &filter.startswith {
-			matches = matches && filename.starts_with(startswith);
-		}
-		if let Some(endswith) = &filter.endswith {
-			matches = matches && filename.ends_with(endswith)
-		}
-		if let Some(contains) = &filter.contains {
-			matches = matches && filename.contains(contains);
-		}
-		matches
+
+		let startswith = self
+			.startswith
+			.iter()
+			.map(|s| TERA.lock().unwrap().render_str(s, &res.context).unwrap())
+			.map(|s| if !self.case_sensitive { s.to_lowercase() } else { s })
+			.any(|mut s| {
+				let mut negate = false;
+
+				if s.starts_with('!') {
+					negate = true;
+					s = s.replacen('!', "", 1);
+				}
+				let mut matches = filename.starts_with(&s);
+				if negate {
+					matches = !matches
+				}
+				matches
+			});
+
+		let endswith = self
+			.endswith
+			.iter()
+			.map(|s| TERA.lock().unwrap().render_str(s, &res.context).unwrap())
+			.map(|s| if !self.case_sensitive { s.to_lowercase() } else { s })
+			.any(|mut s| {
+				let mut negate = false;
+
+				if s.starts_with('!') {
+					negate = true;
+					s = s.replacen('!', "", 1);
+				}
+				let mut matches = filename.ends_with(&s);
+				if negate {
+					matches = !matches
+				}
+				matches
+			});
+
+		let contains = self
+			.contains
+			.iter()
+			.map(|s| TERA.lock().unwrap().render_str(s, &res.context).unwrap())
+			.map(|s| if !self.case_sensitive { s.to_lowercase() } else { s })
+			.any(|mut s| {
+				let mut negate = false;
+
+				if s.starts_with('!') {
+					negate = true;
+					s = s.replacen('!', "", 1);
+				}
+				let mut matches = filename.contains(&s);
+				if negate {
+					matches = !matches
+				}
+				matches
+			});
+
+		startswith || endswith || contains
 	}
 }
 
@@ -52,74 +92,74 @@ mod tests {
 
 	#[test]
 	fn match_beginning_case_insensitive() {
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
 		let filename = Filename {
-			startswith: Some("TE".into()),
+			startswith: vec!["TE".into()],
 			..Default::default()
 		};
-		assert!(filename.matches(&mut path))
+		assert!(filename.matches(&path))
 	}
 
 	#[test]
 	fn match_ending_case_insensitive() {
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
 		let filename = Filename {
-			endswith: Some("DF".into()),
+			endswith: vec!["DF".into()],
 			..Default::default()
 		};
-		assert!(filename.matches(&mut path))
+		assert!(filename.matches(&path))
 	}
 
 	#[test]
 	fn match_containing_case_insensitive() {
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
 		let filename = Filename {
-			contains: Some("ES".into()),
+			contains: vec!["ES".into()],
 			..Default::default()
 		};
-		assert!(filename.matches(&mut path))
+		assert!(filename.matches(&path))
 	}
 
 	#[test]
 	fn no_match_beginning_case_sensitive() {
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
 		let filename = Filename {
 			case_sensitive: true,
-			startswith: Some("TE".into()),
+			startswith: vec!["TE".into()],
 			..Default::default()
 		};
-		assert!(!filename.matches(&mut path))
+		assert!(!filename.matches(&path))
 	}
 
 	#[test]
 	fn no_match_ending_case_sensitive() {
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
 		let filename = Filename {
 			case_sensitive: true,
-			startswith: Some("DF".into()),
+			startswith: vec!["DF".into()],
 			..Default::default()
 		};
-		assert!(!filename.matches(&mut path))
+		assert!(!filename.matches(&path))
 	}
 
 	#[test]
 	fn no_match_containing_case_sensitive() {
-		let mut path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
+		let path = Resource::from_str("$HOME/Downloads/test.pdf").unwrap();
 		let filename = Filename {
 			case_sensitive: true,
-			contains: Some("ES".into()),
+			contains: vec!["ES".into()],
 			..Default::default()
 		};
-		assert!(!filename.matches(&mut path))
+		assert!(!filename.matches(&path))
 	}
 	#[test]
 	fn match_containing_case_sensitive() {
-		let mut path = Resource::from_str("$HOME/Downloads/tESt.pdf").unwrap();
+		let path = Resource::from_str("$HOME/Downloads/tESt.pdf").unwrap();
 		let filename = Filename {
 			case_sensitive: true,
-			contains: Some("ES".into()),
+			contains: vec!["ES".into()],
 			..Default::default()
 		};
-		assert!(filename.matches(&mut path))
+		assert!(filename.matches(&path))
 	}
 }
